407f7e07618edf76d7839bd2123e3deab8a8db61
Matheus <matheusr.unb@gmail.com>
2021-09-16 23:57:38 -0300

    Atualiza diretório public/
    Refatorações
    
    Atualizando imports
    
    Co-authored-by: Ivan Dobbin <ivandinizdobbin2@gmail.com>
    Co-authored-by: Paulo Vitor <paulovitorrocha.unb@gmail.com>

--- sysarq/src/pages/DocumentsRegister/FrequencyRelation.js
+++ sysarq/src/pages/DocumentsRegister/FrequencyRelation.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import DataTable from "../components/DataTable/DataTable";
+import DataTable from "../components/DataTable";
 
 export default function FrequencyRelation() {
 	return <DataTable title="Relação de Frequência" url="frequency-relation/" />;
